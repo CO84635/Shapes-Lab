60f5e3f2b4c12a125bad4dcd57560b0fe77024e2
Owen Ceresa <142851013+CO84635@users.noreply.github.com>
2024-10-08 15:18:42 -0500

    Circle and Shape class added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "circle.hpp"
 
 using namespace std;
 
 int main () {
+    Circle circle = Circle(2.00);
+    cout << "Anticipated 12.57, Experimental: " << circle.getArea() << endl;
+    cout << "Anticipated 12.57, Experimental: "  <<  circle.getPerimeter() << endl;
 
     return 0;
 }
