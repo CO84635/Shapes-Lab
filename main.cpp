dec058497228c50b8f3a63f9caa94d11b9f48b1a
Owen Ceresa <142851013+CO84635@users.noreply.github.com>
2024-10-09 15:07:22 -0500

    Part 1 without most assert statements

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <cassert>
 #include "circle.hpp"
+#include "rectangle.hpp"
+#include "righttriangle.hpp"
 
 using namespace std;
 
 int main () {
     Circle circle = Circle(2.00);
-    cout << "Anticipated 12.57, Experimental: " << circle.getArea() << endl;
-    cout << "Anticipated 12.57, Experimental: "  <<  circle.getPerimeter() << endl;
+    // cout << "Anticipated 12.57, Experimental: " << circle.getArea() << endl;
+    // cout << "Anticipated 12.57, Experimental: "  <<  circle.getPerimeter() << endl;
+
+    Rectangle rectangle = Rectangle(2.00, 4.00);
+    // cout << "Anticipated 8, Experimental: " << rectangle.getArea() << endl;
+    // cout << "Anticipated 12, Experimental: "  <<  rectangle.getPerimeter() << endl;
+
+    RightTriangle right_triangle = RightTriangle(2.00, 4.00);
+    // cout << "Anticipated 4, Experimental: " << right_triangle.getArea() << endl;
+    // cout << "Anticipated 10.47, Experimental: "  <<  right_triangle.getPerimeter() << endl;
+
+    assert(circle.getArea() < 12.57);
+    assert(circle.getPerimeter() < 12.57);
+
+    assert(rectangle.getArea() == 8);
+    assert(rectangle.getPerimeter() == 12);
+
+    assert(right_triangle.getArea() == 4);
+    assert(right_triangle.getPerimeter() < 10.473);
 
     return 0;
 }
